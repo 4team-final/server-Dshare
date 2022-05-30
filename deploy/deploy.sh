ea6a5d780f7b08ab4fd7820286f25898b87e55c3
jaebin1234 <jaebin1234>
2022-05-30 22:19:38 +0900

    feat:프로젝트 초기셋팅

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,7 +5,7 @@
 function stop(){
     sudo echo " "
     sudo echo "Stop process on port: $WAS_SERVER_PORT"
-    sudo fuser -n tcp -k $WAS_SERVER_PORT
+    sudo fuser -n tcp -k $SERVER_PORT
 
     if [ -f $TMP_PATH_NAME ]; then
         sudo rm $TMP_PATH_NAME
