825559106b056d96cbbe5fcfb63fdb1eda2d5375
woohyeoung <dngud951108@gmail.com>
2022-06-12 13:00:13 +0900

    feat:스프링 시큐리티, jjwt gradle추가 및 웹시큐리티 설정 파일 생성 #6

--- src/main/java/com/douzone/server/config/security/WebSecurityConfig.java
+++ src/main/java/com/douzone/server/config/security/WebSecurityConfig.java
@@ -1,4 +1,18 @@
 package com.douzone.server.config.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+	@Bean
+	public SecurityFilterChain securityConfig(HttpSecurity http) throws Exception {
+
+		return http.build();
+	}
 }
