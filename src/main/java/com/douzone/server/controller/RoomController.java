0c7542ea0a68ea19470b45855b062890bc3ad52c
jaebin1234 <jaebin1234>
2022-06-17 00:12:55 +0900

    특정 유저 예약 현황 조회 , 과거 예약, 현재 예약 완료

--- src/main/java/com/douzone/server/controller/RoomController.java
+++ src/main/java/com/douzone/server/controller/RoomController.java
@@ -9,10 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -44,4 +41,10 @@ public class RoomController {
 	public ResponseEntity<ResponseDTO> myReservation(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 		return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ROOM_RESERVE_MY, roomService.myReservation(principalDetails.getEmployee().getId())));
 	}
+
+	@GetMapping("/reservation/{empId}")
+	public ResponseEntity<ResponseDTO> myReservation(@PathVariable("empId") @Valid Long empId) {
+		return ResponseEntity.ok().body(ResponseDTO.of(HttpStatus.OK, Msg.SUCCESS_ROOM_RESERVE_MY, roomService.myReservation(empId)));
+	}
+
 }
