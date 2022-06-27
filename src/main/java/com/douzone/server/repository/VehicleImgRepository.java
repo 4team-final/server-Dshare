efc25171a4a97c8118fd8f512e541372017b2952
woohyeoung <dngud951108@gmail.com>
2022-06-27 18:05:11 +0900

    fix: 차량 사진 업로드 방식 수정

--- src/main/java/com/douzone/server/repository/VehicleImgRepository.java
+++ src/main/java/com/douzone/server/repository/VehicleImgRepository.java
@@ -2,11 +2,14 @@ package com.douzone.server.repository;
 
 import com.douzone.server.entity.VehicleImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface VehicleImgRepository extends JpaRepository<VehicleImg, Long> {
-	Optional<VehicleImg> findByVehicleId(Long id);
+	List<VehicleImg> findByVehicle_Id(Long id);
 
-	void deleteByVehicleId(Long id);
+	@Query("select vi.path from VehicleImg vi where vi.vehicle.id = :id")
+	List<String> findPathByVehicleId(@Param("id") Long id);
 }
