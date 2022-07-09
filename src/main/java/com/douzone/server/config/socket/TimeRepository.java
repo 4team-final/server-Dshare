fdadf920cab2cd6fdf87aacdab4af72f2abc597a
woohyeoung <dngud951108@gmail.com>
2022-07-09 19:44:43 +0900

    fix: 웹소켓 시간 테이블 삭제 수정

--- src/main/java/com/douzone/server/config/socket/TimeRepository.java
+++ src/main/java/com/douzone/server/config/socket/TimeRepository.java
@@ -1,9 +1,12 @@
 package com.douzone.server.config.socket;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TimeRepository extends JpaRepository<Time, Long> {
@@ -11,4 +14,7 @@ public interface TimeRepository extends JpaRepository<Time, Long> {
 	List<Time> findByCalendar_Uid(String uid);
 
 	List<Time> findByCalendar_UidAndRoomId(String uid, Integer roomId);
+
+	@Query("select tv from Time tv where tv.calendar.uid = :uid and tv.roomId = :rid")
+	Optional<List<Time>> selectByUidAndVid(@Param("uid") String uid, @Param("rid") Integer rid);
 }
